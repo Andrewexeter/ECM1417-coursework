50fc1f5a7cdce1f1d2468c73640e69c0db450387
Andrewexeter <129608903+Andrewexeter@users.noreply.github.com>
2023-04-05 14:37:35 +0100

    Add files via upload

---
+++ coursework/header.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <style>
+			ul.menu {
+				list-style-type: none;
+				padding: 0px;
+				background-color: rgb(0, 83, 161);
+				overflow: hidden;
+			}
+			ul.menu li {
+				float: left;
+			}
+			ul.menu li a {
+				display: block;
+				width: 5cm;
+				height: .7cm;
+				padding-top: 12px;
+				color: white;
+				text-align: center;
+				text-decoration: none;				
+			}
+			ul.menu li a:hover {
+			  background-color: rgb(13, 140, 255);
+			}
+		</style>
+    </head>
+    <body style = "background:url(carl-raw-m3hn2Kn5Bns-unsplash.jpg)">
+    <nav>
+    <ul class="menu">
+			<li><a href="index.php">home</a></li>
+			<li><a href="#ecm1407">memory</a></li>
+			<li><a href="#ecm1410"></a></li>						
+		</ul>
+    </nav>
+    </body>        
+</html>
